5af0806cd86a4e3faeb8b6bd58a4a5f7261c9286
alexis mouchon <alexismouchon09@gmail.com>
2022-10-28 10:13:44 +0200

    Operational CORS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
 
-import { LoginComponent } from './login/login.component';
-import { BlankComponent } from './blank/blank.component';
-import { ErrorBoxComponent } from './error-box/error-box.component';
-import { RegisterComponent } from './register/register.component';
-import { MapComponent } from './map/map.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {BlankComponent} from './blank/blank.component';
+import {ErrorBoxComponent} from './error-box/error-box.component';
+import {RegisterComponent} from './register/register.component';
+import {MapComponent} from './map/map.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {HomeComponent} from './home/home.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -30,9 +31,11 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {};
+export class AppModule {
+};
 
